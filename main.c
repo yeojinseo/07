938dfd37f7a181aa3926e25c03bc92cc42f5cdbb
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-10-18 17:03:00 +0900

    05

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void f(void);
+void sub(void);
 
-int i;
 int main(int argc, char *argv[]) {
-	
-	for(i=0;i<5;i++)
-	{
-	f();
-	}
+	int i;
+	for(i=0;i<3;i++)
+		sub();
 	return 0;
 } 
 
-void f(void)
+void sub(void)
 {
-	for(i=0;i<10;i++)
-		printf("#");
+	int auto_count = 0; //자동할당 
+	static int static_count = 0; //정적할당
+	auto_count++;
+	static_count++;
+	printf("auto_count=%d\n", auto_count);
+	printf("static_count=%d\n", static_count);
 }
