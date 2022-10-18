a42bbb91f3115113b044f10a5716507f68c5b363
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-10-18 16:29:00 +0900

    02

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int flag = 1;
-	while (flag!=0)
+	int i;
+	
+	for (i=0;i<5;i++)
 	{
-		int y;
-		y = 3;
-		flag = 0;
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
 	}
 	
 	return 0;
-}
+} 
